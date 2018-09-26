fa8cfbb2a382f80329317b89c8fc90c7fc8b89a7
Sai Kishore <sai2009kishore@gmail.com>
2018-09-26 14:06:01 +0530

    Removed City and added Embedded Address

---
+++ app/models/Address.java
@@ -0,0 +1,53 @@
+package models;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+	private String area;
+	private String city;
+	private String state;
+	private String country;
+	private String pinCode;
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+}
--- app/models/Person.java
+++ app/models/Person.java
@@ -1,62 +1,60 @@
 package models;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "person")
 public class Person {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-    @Column(nullable = false)
-    private String name;
-    
-    private int age;
-    
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    private City city;
-    
-    // Setters
-    public void setId(int id) {
-    	this.id = id;
-    }
-    
-    public void setName(String name) {
-    	this.name = name;
-    }
-    
-    public void setAge(int age) {
-    	this.age = age;
-    }
-    
-    public void setCity(City city) {
-    	this.city = city;
-    }
-    
-    // Getters
-    public int getId() {
-    	return this.id;
-    }
-    
-    public String getName() {
-    	return this.name;
-    }
-    
-    public int getAge() {
-    	return this.age;
-    }
-    
-    public City getCity() {
-    	return this.city;
-    }
+	@Column(nullable = false)
+	private String name;
+
+	private int age;
+
+	@Embedded
+	private Address address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+
 }
