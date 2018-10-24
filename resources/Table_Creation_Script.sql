834aa1a52c3644818e19cea64c304c51a9ca9986
Sai Kishore <sai2009kishore@gmail.com>
2018-10-24 17:20:57 +0530

    Table creation script

---
+++ resources/Table_Creation_Script.sql
@@ -0,0 +1,11 @@
+create table person(
+	id int primary key auto_increment, 
+	name varchar(100), 
+	age int, 
+	area varchar(100), 
+	city varchar(100), 
+	state varchar(100), 
+	country varchar(100), 
+	pinCode varchar(100)
+);
+
